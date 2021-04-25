4ef98658970e80faa68ea62bdca84915dc20dcc1
Miguel <35431824+Mig-uel@users.noreply.github.com>
2021-04-25 18:08:54 -0400

    Add files via upload

--- RecycleIt/AddALocation/AddLocation/addlocation.php
+++ RecycleIt/AddALocation/AddLocation/addlocation.php
@@ -12,7 +12,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
   die("Connection failed: ". $conn->connect_error);
 }
-echo "Connected successfully";
+// echo "Connected successfully";
 
 $address = $_POST['address'];
 $location = $_POST['lName'];
@@ -22,25 +22,10 @@ $plastic = $_POST['plastic'];
 $cans = $_POST['cans'];
 $glass = $_POST['glass'];
 
-echo "<br />";
-echo $address;
-echo "<br />";
-echo $location;
-echo "<br />";
-echo $name;
-echo "<br />";
-echo $date;
-echo "<br />";
-echo $plastic;
-echo "<br />";
-echo $cans;
-echo "<br />";
-echo $glass;
-
 $sql = "INSERT INTO Info (address, lName, nPerson, tDate, plastic, cans, glass) VALUES ('$address', '$location', '$name', '$date', '$plastic', '$cans', '$glass')";
 
 if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
+  echo "";
 } else {
   echo "Error: " . $sql . "<br>" . $conn->error;
 }
@@ -48,3 +33,50 @@ if ($conn->query($sql) === TRUE) {
 $conn->close();
 
 ?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>Location Added</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <style>
+    p {
+    color: white;
+    }
+    body {
+      background-image: url("https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1652&q=80");
+      background-size: cover;
+      background-repeat: no-repeat;
+      background-position: center;
+      background-attachment: fixed;
+    }
+    .row {
+      position: relative;
+      text-align: center;
+      height: 100vh;
+    }
+    .row .col-md-12 {
+      height: 40px;
+      top: 40%;
+    }
+    h1 {
+      padding-bottom: 20px;
+      color: whitesmoke;
+    }
+  </style>
+
+</head>
+<body>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12">
+        <h1>Thank you!</h1>
+        <p>Thank you for submitting a location.</p>
+        <a href="RecycleIt"><button type="button" class="btn btn-outline-primary">Return Home</button></a>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
